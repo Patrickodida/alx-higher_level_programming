40b7696a77299d00003538cee9271fd1a6d6a9e9
Patrickodida <patrick.o.odida@gmail.com>
2023-03-24 15:42:49 -0700

    SQL more queries tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,18 +1,20 @@
 -- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter.
+-- Not linked to the show Dexter.
+-- Records are sorted by ascending genre name.
 SELECT DISTINCT `name`
-FROM `tv_genres` AS g
-INNER JOIN `tv_show_genres` AS s
-ON g.`id` = s.`genre_id`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
 
-INNER JOIN `tv_shows` AS t
-ON s.`show_id` = t.`id`
-WHERE g.`name` NOT IN
-(SELECT `name`
-	FROM `tv-genres` AS g
-	INNER JOIN `tv_show_genres` AS s
-	ON g.`id` = s.`genre_id`
+       INNER JOIN `tv_shows` AS t
+       ON s.`show_id` = t.`id`
+       WHERE g.`name` NOT IN
+             (SELECT `name`
+		FROM `tv_genres` AS g
+	             INNER JOIN `tv_show_genres` AS s
+	             ON g.`id` = s.`genre_id`
 
-	INNER JOIN `tv_shows` AS t
-	ON s.`show_id` = t.`id`
-	WHERE t.`title` = "Dexter"
-	ORDER BY g.`name`;
+	             INNER JOIN `tv_shows` AS t
+	             ON s.`show_id` = t.`id`
+	             WHERE t.`title` = "Dexter")
+ORDER BY g.`name`;
--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,8 @@
 -- lists all shows contained in the database hbtn_0d_tvshows.
+-- Display NULL for shows without genre.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre-id.
 SELECT s.`title`, g.`genre_id`
-FROM `tv_shows` AS s
-LEFT JOIN `tv_show_genres` AS g
-ON s.`id` = g.`show-id`
+  FROM `tv_shows` AS s
+     LEFT JOIN `tv_show_genres` AS g
+     ON s.`id` = g.`show_id`
 ORDER BY s.`title`, g.`genre_id`;
